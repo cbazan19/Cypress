4ec23a912930d08b514b71d22f4b968fa9972d32
Carlos <carlosbazan@hotmail.com>
2023-10-26 12:32:46 -0300

    Subiendo carpeta de Cypress

---
+++ Jenkinsfile
@@ -0,0 +1,38 @@
+pipeline{
+    agent any
+
+    parameters{
+        string(name:"SPEC", defaultValue: "cypress/e2e/1-getting-started/**", description: "Ejemplo: cypress/e2e/1-getting-started/abbaco_smoke_prod.cy.js")
+        choice(name: "BROWSER", choises: ['Chrome', 'Edge', 'Firefox'], description: "Elegir browser para ejecutar el test")
+    }
+
+    options{
+        ansiColor('xterm')
+    }
+
+    stages{
+        stage('Build'){
+            steps{
+                echo "Building application"
+            }
+        }
+        stage('Testing'){
+            steps
+                bat "npm i"
+                bat "npx cypress run --browser ${BROWSER} --spec ${SPEC}"
+        }
+        stage('Deploy'){
+            steps
+                echo "Deploying the application"
+        }
+    }
+
+    post{
+        always{
+            publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: 'cypress/report', reportFiles: 'index.html', reportName: 'HTML Report', reportTitles: ''])
+        }
+
+    }
+
+
+}
---
+++ cypress/support/commands.js
@@ -0,0 +1,44 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import 'cypress-file-upload';
+
+Cypress.Commands.add("LoginAbbaco", (user, password) => 
+{ 
+    
+    cy.visit('https://bonds.mercapabbaco.com/')
+    cy.origin('https://mercap.auth0.com', () => 
+    {
+
+      cy.viewport(1600, 900)
+      cy.get('#username').should('exist').and('be.visible').click().type('cy_smoke@mercapsoftware.com')
+      cy.get('#password').should('exist').and('be.visible').click().type('testing1')
+      cy.contains('Continue').click({force: true})
+
+
+   })
+
+})
