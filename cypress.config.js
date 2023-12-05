ac8917f5bc5d154411194270a8c2227b254e1244
Carlos <carlosbazan@hotmail.com>
2023-12-05 00:11:41 -0300

    Habilitando plug in mocha y poniendo run en headless

--- cypress.config.js
+++ cypress.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
     experimentalSesssionAndOrigin: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(off);
+      require('cypress-mochawesome-reporter/plugin')(on);
       on("before:browser:launch", (browser = {}, launchOptions) => {
         prepareAudit(launchOptions);
       });
